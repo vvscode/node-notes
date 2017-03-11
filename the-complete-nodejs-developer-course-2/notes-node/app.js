f5a523b1f4d1fa9027e7ecf5cbdf2d1914b7252c
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-03-11 22:01:00 +0300

    hello world

---
+++ the-complete-nodejs-developer-course-2/notes-node/app.js
@@ -0,0 +1,8 @@
+console.log('Starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+console.log(user);
+fs.appendFile('greetings.txt', `Hello ${user.username}!`);
