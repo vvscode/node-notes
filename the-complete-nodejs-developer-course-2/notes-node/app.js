b7eb9ac137e70043113125bd1c06e9dbde02ad62
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-03-11 22:09:11 +0300

    About require

--- the-complete-nodejs-developer-course-2/notes-node/app.js
+++ the-complete-nodejs-developer-course-2/notes-node/app.js
@@ -1,8 +1,14 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
-let user = os.userInfo();
-console.log(user);
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+// let user = os.userInfo();
+// console.log(user);
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You're ${notes.age}`;
+
+let res = notes.addNote();
+
+console.log(res);
+console.log(notes.add(1, 45));
---
+++ the-complete-nodejs-developer-course-2/notes-node/notes.js
@@ -0,0 +1,8 @@
+console.log('starting Notes.js');
+
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
+
+module.exports.add = (a, b) => a + b;
