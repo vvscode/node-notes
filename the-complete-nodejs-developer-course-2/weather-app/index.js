9b3f44b5b8b4dd807dd0efc2d282069e82284463
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-03-22 23:28:50 +0300

    Display address from response

--- the-complete-nodejs-developer-course-2/weather-app/index.js
+++ the-complete-nodejs-developer-course-2/weather-app/index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const util = require('util');
 
 const address = `3й Железнодорожный переулок 15`;
 const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`;
@@ -7,5 +8,5 @@ request.get({
   url,
   json: true
 }, (err, response, body) => {
-  console.log(body);
+  console.log(`Address: ` , JSON.stringify(body.results[0].formatted_address, undefined, 2));
 });
