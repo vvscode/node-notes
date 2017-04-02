05ecb11f76543ba4c6dd8d5b67d3364fd70a3d51
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-02 11:04:01 +0300

    Get address from cmd

--- the-complete-nodejs-developer-course-2/weather-app/index.js
+++ the-complete-nodejs-developer-course-2/weather-app/index.js
@@ -1,13 +1,26 @@
 const request = require('request');
 const util = require('util');
+const yargs = require('yargs');
 
-const address = `3й Железнодорожный переулок 15`;
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Adress to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .argv;
+
+const address = argv.a;
 const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`;
 
 request.get({
   url,
   json: true
 }, (err, response, body) => {
-  console.log(`Address: ` , JSON.stringify(body.results[0].formatted_address, undefined, 2));
-  console.log(`Coordinates: ` , JSON.stringify(body.results[0].geometry.location.lat), JSON.stringify(body.results[0].geometry.location.lng));
+  console.log(`Address: `, JSON.stringify(body.results[0].formatted_address, undefined, 2));
+  console.log(`Coordinates: `, JSON.stringify(body.results[0].geometry.location.lat), JSON.stringify(body.results[0].geometry.location.lng));
 });
