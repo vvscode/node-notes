94bc966b97323462810d9b721c058f885fb77cb2
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-07 22:35:36 +0300

    Send json / send error

--- the-complete-nodejs-developer-course-2/hello-express/app.js
+++ the-complete-nodejs-developer-course-2/hello-express/app.js
@@ -8,5 +8,13 @@ app.get('/', (req, res) => {
   res.send('Hello from express.js');
 });
 
+app.get('/about', (req, res) => res.send({
+  text: 'Created via Express.js'
+}));
+
+app.get('/bad', (req, res) => res.status(500).send({
+  errorMessage: 'Incorrect request handled by /bad'
+}));
+
 app.listen(HTTP_PORT);
 console.log(`Server started at :${HTTP_PORT}`);
