d296b91338879120cf3fa404048c4b8fd8582136
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-08 09:08:52 +0300

    Use handlebars template engine

--- the-complete-nodejs-developer-course-2/hello-express/app.js
+++ the-complete-nodejs-developer-course-2/hello-express/app.js
@@ -1,8 +1,11 @@
 const HTTP_PORT = 3000;
 
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+app.set('view engine', 'hbs');
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -10,12 +13,20 @@ app.get('/', (req, res) => {
   res.send('Hello from express.js');
 });
 
-app.get('/about', (req, res) => res.send({
-  text: 'Created via Express.js'
-}));
+app.get('/about', (req, res) =>
+  // Both will work  
+  // res.render('about', {
+  res.render('about.hbs', {
+    text: 'Created via Express.js',
+    options: [
+      'one',
+      'two',
+      'three'
+    ]
+  }));
 
 app.get('/bad', (req, res) => res.status(500).send({
-  errorMessage: 'Incorrect request handled by /bad'
-}));
+    errorMessage: 'Incorrect request handled by /bad',
+  }));
 
 app.listen(HTTP_PORT, () => console.log(`Server started at :${HTTP_PORT}`));
