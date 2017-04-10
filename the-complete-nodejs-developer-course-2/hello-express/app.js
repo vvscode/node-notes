5e9c12732c6fa159b856d005cdb1ed36a1132536
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-10 05:28:38 +0300

    Use middleware for maintenance page

--- the-complete-nodejs-developer-course-2/hello-express/app.js
+++ the-complete-nodejs-developer-course-2/hello-express/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   let now = new Date().toString();
   let log = `${now} : ${req.method} ${req.url}`;
@@ -16,6 +16,12 @@ app.use((req, res, next) => {
   fs.appendFile('server.log', log + '\n', (err) => err && console.error('Unable to write log'));
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(`${__dirname}/public`));
 
 hbs.registerHelper('currentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => (text || '').toUpperCase());
