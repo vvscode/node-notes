994464521be42eb6922ee3c1e3526e4551d6fac1
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-12 23:42:07 +0300

    Add square tests

--- the-complete-nodejs-developer-course-2/node-tests/utils/utils-test.js
+++ the-complete-nodejs-developer-course-2/node-tests/utils/utils-test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const {
-  add
+  add,
+  square
 } = require('./utils');
 
 describe('Utils', function() {
@@ -11,6 +12,15 @@ describe('Utils', function() {
       assert.equal(add(6, 6), 6 + 6);
       assert.equal(add(9, 0), 9 + 0);
       assert.equal(add(5, 1), 5 + 1);
+    });
+  });
+  describe('#square()', function() {
+    it('should return square of param', function() {
+      assert.equal(square(0), 0);
+      assert.equal(square(1), 1);
+      assert.equal(square(2), 4);
+      assert.equal(square(5), 25);
+      assert.equal(square(19), 361);
     });
   });
 });
