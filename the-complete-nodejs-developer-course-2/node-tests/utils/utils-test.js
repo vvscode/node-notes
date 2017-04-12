b6c95052d7a1895f460c92fa49ac6f15697abb71
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-12 23:37:20 +0300

    Add more asserts

--- the-complete-nodejs-developer-course-2/node-tests/utils/utils-test.js
+++ the-complete-nodejs-developer-course-2/node-tests/utils/utils-test.js
@@ -7,6 +7,10 @@ describe('Utils', function() {
   describe('#add()', function() {
     it('should return sum of arguments', function() {
       assert.equal(add(1, 2), 1 + 2);
+      assert.equal(add(0, 3), 0 + 3);
+      assert.equal(add(6, 6), 6 + 6);
+      assert.equal(add(9, 0), 9 + 0);
+      assert.equal(add(5, 1), 5 + 1);
     });
   });
 });
