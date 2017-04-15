9c4749c278fd65437c06d21278eb1b2fddd077c0
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-15 21:52:14 +0300

    Create express server for testing

---
+++ the-complete-nodejs-developer-course-2/node-tests/server/server.js
@@ -0,0 +1,10 @@
+const PORT = 3000;
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(PORT, () => console.log(`0.0.0.0:${PORT}`));
