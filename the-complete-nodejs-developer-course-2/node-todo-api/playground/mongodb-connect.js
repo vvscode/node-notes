0d7b9a1d5691635aaf30343d9000a4027b54e0b8
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-04-18 22:45:27 +0300

    Insert data with mongodb

--- the-complete-nodejs-developer-course-2/node-todo-api/playground/mongodb-connect.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/playground/mongodb-connect.js
@@ -6,5 +6,14 @@ MongoClient
       return console.log('Unable to connecto to MongoDB server', err);
     }
     console.log('Connected to MongoDB');
-    db.close();
+
+    db.collection('Todos').insertOne({
+      text: `Check DB insetion at ${new Date()}`
+    }, (err, result) => {
+      if (err) {
+        return console.log('Error on inserting todo', err);
+      }
+      console.log('Todo was inserted', JSON.stringify(result.ops, undefined, 2));
+      db.close();
+    });
   });
