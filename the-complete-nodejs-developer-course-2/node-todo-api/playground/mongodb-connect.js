6817572ce083d4653114c29b7145d09411690c59
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-04-20 23:10:27 +0300

    Create promise-based wrappers

--- the-complete-nodejs-developer-course-2/node-todo-api/playground/mongodb-connect.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/playground/mongodb-connect.js
@@ -1,19 +1,29 @@
 const MongoClient = require('mongodb');
 
-MongoClient
-  .connect(`mongodb://localhost:27017/TodoApp`, (err, db) => {
-    if (err) {
-      return console.log('Unable to connecto to MongoDB server', err);
-    }
-    console.log('Connected to MongoDB');
+let lastDbConnection;
 
-    db.collection('Todos').insertOne({
-      text: `Check DB insetion at ${new Date()}`
-    }, (err, result) => {
-      if (err) {
-        return console.log('Error on inserting todo', err);
-      }
-      console.log('Todo was inserted', JSON.stringify(result.ops, undefined, 2));
-      db.close();
-    });
-  });
+const connect = (link) => new Promise(
+  (resolve, reject) => MongoClient
+    .connect(link, (err, db) => err 
+      ? reject('Unable to connecto to MongoDB server', err)
+      : resolve(lastDbConnection = db))
+);
+
+const insertOne = (db, collectionName, data) => new Promise(
+  (resolve, reject) => db.collection(collectionName)
+    .insertOne(data, (err, result) => err ? reject(err) : resolve(result))
+);
+
+connect(`mongodb://localhost:27017/TodoApp`)
+  .then((db) => {
+    lastDbConnection = db;
+    console.log('Connected to MongoDB');
+    return insertOne(db, 'Todos', {
+      text: `Check DB insetion at ${new Date()}`,
+      done: false
+    })
+      .then((result) => console.log('Todo was inserted', JSON.stringify(result.ops, undefined, 2)))
+      .then(() => db);
+  })
+  .catch(err => console.error(err))
+  .then(() => lastDbConnection.close());
