d1754c462e34422d6a6d28cdd546ab644f033967
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-04-28 22:18:37 +0300

    Add some defaults / validations

--- the-complete-nodejs-developer-course-2/node-todo-api/server.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/server.js
@@ -5,30 +5,29 @@ mongoose.Promise = global.Promise;
 // Connection settings
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+let TodoSchema
+
 let Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false,
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: Date.now
   }
 });
 
-let newTodo = new Todo({
-  text: 'First Todo from mongoose'
-});
-newTodo.save().then(
-  (todo) => console.log('Saved todo:', todo),
-  (e) => console.error('Error on saving', e)
-);
 
 (new Todo({
   text: 'Another Todo from mongoose',
-  completed: true,
-  completedAt: Date.now()
+  // completed: true,
+  // completedAt: Date.now()
 })).save()
   .then(
     (todo) => console.log('Saved todo:', todo),
