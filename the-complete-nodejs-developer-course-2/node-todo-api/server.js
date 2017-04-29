1f43c76353c5220d2c3bcc24432e70d344ae09d9
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-04-29 22:06:04 +0300

    INstall express. Add first endpoint for todo creation

--- the-complete-nodejs-developer-course-2/node-todo-api/server.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/server.js
@@ -1,13 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
 
-(new User({
-  email: 'some@email',
-  // completed: true,
-  // completedAt: Date.now()
-})).save()
-  .then(
-    (todo) => console.log('Saved todo:', todo),
-    (e) => console.error('Error on saving', e)
-  );
+const PORT = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res, next) => {
+  console.log('POST /todos', req.body);
+
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save()
+    .then((doc) => res.send(doc))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.listen(PORT, () => console.log(`Server started at http://127.0.0.1:${PORT}`));
