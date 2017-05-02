d4d62b2337279b6f4603d52ef2417ef5d20b5704
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-02 05:37:57 +0300

    Add get for /posts

--- the-complete-nodejs-developer-course-2/node-todo-api/server.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/server.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('./db/mongoose');
-let { User } = require('./models/user');
-let { Todo } = require('./models/todo');
+const {
+  mongoose
+} = require('./db/mongoose');
+let {
+  User
+} = require('./models/user');
+let {
+  Todo
+} = require('./models/todo');
 
 const PORT = 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res, next) => {
-  console.log('POST /todos', req.body);
-
+app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
   });
   todo.save()
     .then((doc) => res.send(doc))
     .catch((err) => res.status(400).send(err));
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find({})
+    .then((todos) => res.send({ todos }))
+    .catch((err) => res.status(500).send(err));
 });
 
 app.listen(PORT, () => console.log(`Server started at http://127.0.0.1:${PORT}`));
