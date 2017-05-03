be484b45ed1b7231a84e79675284434d159d23b1
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-04 00:02:16 +0300

    Validate object id

--- the-complete-nodejs-developer-course-2/node-todo-api/playground/mongoose-queries.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/playground/mongoose-queries.js
@@ -1,8 +1,13 @@
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('../db/mongoose');
 const {Todo} = require('../models/todo');
 
-
 let id = `5907f2a5cae1bb504c6e5a2f`;
+
+
+if (!ObjectID.isValid(id)) {
+  console.log(`${id} is not valid`);
+}
 
 Todo.find({
   _id: id
