700003f76d88e6453f72f3175b05965a6290f2c0
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-10 22:13:22 +0300

    Add tests for /todos/:id

--- the-complete-nodejs-developer-course-2/node-todo-api/server.test.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/server.test.js
@@ -1,5 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const {
   app
@@ -78,6 +79,41 @@ describe('/todos', () => {
         .get('/todos')
         .expect(200)
         .expect((res) => expect(res.body.todos.length).toBe(todos.length))
+        .end((err) => done(err))
+    });
+  });
+
+  describe('/:id GET ', () => {
+    let time = new Date();
+    let _id = new ObjectID();
+    const todo = { text: 'Existed todo', _id };
+    beforeEach((done) => {
+      Todo.remove({}).then(() => Todo.insertMany([todo])).then(() => done());
+    });
+
+    it('should return todo', (done) => {
+      request(app)
+        .get(`/todos/${_id}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.text).toEqual(todo.text);
+          expect(res.body._id).toEqual(todo._id);
+        })
+        .end((err) => done(err))
+    });
+
+    it('should return 404 for unknown id', (done) => {
+      request(app)
+        .get(`/todos/${`${_id}`.replace(/\w/g, '0')}`)
+        .expect(404)
+        .end((err) => done(err))
+    });
+
+
+    it('should return 400 for invalid is', (done) => {
+      request(app)
+        .get(`/todos/1234`)
+        .expect(400)
         .end((err) => done(err))
     });
   });
