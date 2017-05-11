dc066bfd5d0d9ea74cfcd4dc09322982711ca6d9
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-11 23:27:55 +0300

    Read mongodb url from process for heroku

--- the-complete-nodejs-developer-course-2/node-todo-api/db/mongoose.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Use native promises
 mongoose.Promise = global.Promise;
 // Connection settings
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
