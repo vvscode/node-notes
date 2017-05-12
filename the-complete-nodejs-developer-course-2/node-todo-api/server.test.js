3cf863d406d5f080ef25b71e207ce5f91353ca5c
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-12 22:48:06 +0300

    Add DELETE tests

--- the-complete-nodejs-developer-course-2/node-todo-api/server.test.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/server.test.js
@@ -1,6 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
-const { ObjectID } = require('mongodb');
+const {
+  ObjectID
+} = require('mongodb');
 
 const {
   app
@@ -86,7 +88,10 @@ describe('/todos', () => {
   describe('/:id GET ', () => {
     let time = new Date();
     let _id = new ObjectID();
-    const todo = { text: 'Existed todo', _id };
+    const todo = {
+      text: 'Existed todo',
+      _id
+    };
     beforeEach((done) => {
       Todo.remove({}).then(() => Todo.insertMany([todo])).then(() => done());
     });
@@ -113,6 +118,54 @@ describe('/todos', () => {
     it('should return 400 for invalid is', (done) => {
       request(app)
         .get(`/todos/1234`)
+        .expect(400)
+        .end((err) => done(err))
+    });
+  });
+
+  describe('/:id DELETE ', () => {
+    let time = new Date();
+    let _id = new ObjectID();
+    const todo = {
+      text: 'Existed todo',
+      _id
+    };
+    beforeEach((done) => {
+      Todo.remove({}).then(() => Todo.insertMany([todo])).then(() => done());
+    });
+
+    it('should remove todo and return removed todo', (done) => {
+      request(app)
+        .delete(`/todos/${_id}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.text).toEqual(todo.text);
+          expect(res.body._id).toEqual(todo._id);
+        })
+        .end((err) => {
+          if (err) {
+            done(err);
+          }
+          Todo.find()
+            .then((todos) => {
+              expect(todos.length).toBe(0);
+              done();
+            })
+            .catch((err) => done(err));
+        });
+    });
+
+    it('should return 404 for non-existing id', (done) => {
+      request(app)
+        .delete(`/todos/${_id.toHexString().replace(/\w/g, '0')}`)
+        .expect(404)
+        .end((err) => done(err))
+    });
+
+
+    it('should return 400 for invalid id', (done) => {
+      request(app)
+        .delete(`/todos/1234`)
         .expect(400)
         .end((err) => done(err))
     });
