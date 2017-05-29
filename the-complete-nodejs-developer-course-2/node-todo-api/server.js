fe135f0d7d8d1aa1c4f9a594ada6fb6abfaea5e1
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-05-29 18:44:53 +0300

    Setup separate test environment in scripts

--- the-complete-nodejs-developer-course-2/node-todo-api/server.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  process.env.PORT = 3000;
+} else if (env === 'test') {
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TestTodoApp';
+  process.env.PORT = 3001;
+}
+
 
 const {
   mongoose
