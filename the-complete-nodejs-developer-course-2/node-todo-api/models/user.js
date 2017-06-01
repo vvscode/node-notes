45b25bbfca4521498675c84e5d5ea082bf886b54
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-01 23:36:15 +0300

    Setup user model. Add validator dependency

--- the-complete-nodejs-developer-course-2/node-todo-api/models/user.js
+++ the-complete-nodejs-developer-course-2/node-todo-api/models/user.js
@@ -1,16 +1,41 @@
 const { mongoose } = require('../db/mongoose');
+const validator = require('validator');
 
+// {
+//   email: 'some@mail.ru',
+//   password: '12342135',
+//   tokens: [
+//     { access: 'phone', token: 'some token' },
+//     { access: 'home', token: 'some another token' }
+//   ]
+// }
 let User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     minlength: 1,
     trim: true,
+    unique: true,
     validate: {
-      validator: (value) => /\w@\w/.test(value),
+      validator: validator.isEmail,
       message: '{VALUE} should be valid email',
     },
-  }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }],
 });
 
 module.exports = { User };
--- the-complete-nodejs-developer-course-2/node-todo-api/typings/index.d.ts
+++ the-complete-nodejs-developer-course-2/node-todo-api/typings/index.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="globals/mongoose/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/validator/index.d.ts" />
