e93ccc4660cfd04be7af108ce615f88d4de1c96f
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-02-02 01:52:09 +0300

    Use dynamic port for heroku

--- node-with-react-fullstack-web-development/server/index.js
+++ node-with-react-fullstack-web-development/server/index.js
@@ -1,10 +1,12 @@
 // @ts-check
 const express = require("express");
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-app.listen(5000);
+app.listen(PORT, () => console.log(`Go to http://0.0.0.0:${PORT}`));
