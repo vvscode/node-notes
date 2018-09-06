c50ae1cd09a20a313abb940a289199c4f9974f17
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-09-06 12:36:50 +0400

    Parse request url

--- pirple--pure-node/rest-api/index.js
+++ pirple--pure-node/rest-api/index.js
@@ -4,12 +4,26 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 const PORT = 3000;
 
 // The server should respons to all requests with a string
 const server = http.createServer((req, res) => {
-  res.end('Hello world');
+  // Get the url and parse it
+  // https://developer.mozilla.org/en-US/docs/Web/API/URL
+  const parsedUrl = url.parse(req.url, true);
+
+  // get the path from url
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLHyperlinkElementUtils/pathname
+  const path = parsedUrl.pathname;
+  const trimedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // send the response
+  res.end(`Hello world: ${path} (${trimedPath})`);
+
+  //  log url to console
+  console.log(`Request recieved on path: ${trimedPath}`);
 });
 
 // Start server and listen on port
